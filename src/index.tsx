1cbc43ec18603c686b7e9952621353698a07e6cc
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-09-18 18:08:04 +0500

    feat

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-//Bootstrap import
+import './index.scss';
+import App from './App'
+//Material Design Bootstrap import
 import '@fortawesome/fontawesome-free/css/all.min.css'; 
 import 'bootstrap-css-only/css/bootstrap.min.css'; 
 import 'mdbreact/dist/css/mdb.css';
