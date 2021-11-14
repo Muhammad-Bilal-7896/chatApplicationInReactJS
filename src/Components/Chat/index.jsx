406e810a4a55e139eb8db8c9a0eaa237b716e999
Muhammad-Bilal-7896 <bilalmohib7896@gmail.com>
2021-11-14 21:49:32 +0500

    feat

--- src/Components/Chat/index.jsx
+++ src/Components/Chat/index.jsx
@@ -165,6 +165,55 @@ const Chat = () => {
             </div>
           </div>
         </div>
+        <button
+          type="button"
+          class="btn btn-primary"
+          data-mdb-toggle="modal"
+          data-mdb-target="#AddContactModal"
+          data-mdb-whatever="@getbootstrap"
+        >
+          Open modal for @getbootstrap
+        </button>
+
+        <div
+          class="modal fade"
+          id="AddContactModal"
+          tabindex="-1"
+          aria-labelledby="AddContactModalLabel"
+          aria-hidden="true"
+        >
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+                <button
+                  type="button"
+                  class="btn-close"
+                  data-mdb-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div class="modal-body">
+                <form>
+                  <div class="mb-3">
+                    <label for="recipient-name" class="col-form-label">Recipient:</label>
+                    <input type="text" class="form-control" id="recipient-name" />
+                  </div>
+                  <div class="mb-3">
+                    <label for="message-text" class="col-form-label">Message:</label>
+                    <textarea class="form-control" id="message-text"></textarea>
+                  </div>
+                </form>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-mdb-dismiss="modal">
+                  Close
+                </button>
+                <button type="button" class="btn btn-primary">Send message</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </Sidebar>
 
     </>
